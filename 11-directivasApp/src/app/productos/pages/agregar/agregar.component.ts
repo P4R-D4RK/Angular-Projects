baea538bbe21b1b8fa58da00348dd9ab8cff66ae
Luis Polina <luis.282001@gmail.com>
2022-08-17 16:25:53 -0500

    Directives done

--- 11-directivasApp/src/app/productos/pages/agregar/agregar.component.ts
+++ 11-directivasApp/src/app/productos/pages/agregar/agregar.component.ts
@@ -7,6 +7,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [],
 })
 export class AgregarComponent {
+  mensaje: string = 'Este campo es necesario';
+  color: string = 'red';
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', Validators.required],
   });
@@ -15,5 +18,16 @@ export class AgregarComponent {
 
   tieneError(campo: string): boolean {
     return this.miFormulario.get(campo)?.invalid || false;
+  }
+
+  cambiarNombre() {
+    this.mensaje = Math.random().toString(36).substring(0,10); 
+  }
+
+  cambiarColor() {
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    this.color = color;
   }
 }
