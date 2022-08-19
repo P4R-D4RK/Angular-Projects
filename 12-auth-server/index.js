3fbc38667a890061cae16a8523adc56f2efa03e8
Luis Polina <luis.282001@gmail.com>
2022-08-19 12:32:44 -0500

    App Routes

--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,4 +1,13 @@
+express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+// Routes
+app.use( '/api/auth', require('./routes/auth') )
 
 
-
-console.log('Hola desde Node!');
+app.listen(port, () => {
+    console.log(`Server running on the port ${port}`);
+})
