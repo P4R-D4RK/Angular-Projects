87e42e6f10578cedb945421e657ab33c8c48e5b3
Luis Polina <luis.282001@gmail.com>
2022-08-25 17:33:42 -0500

    Login

--- 12-auth-server/controllers/auth.js
+++ 12-auth-server/controllers/auth.js
@@ -47,13 +47,52 @@ const crearUsuario = async (req, res = response) => {
   }
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async(req, res = response) => {
+  
   const { email, password } = req.body;
 
-  return res.json({
-    ok: true,
-    msg: "Login de usuario /",
-  });
+  try {
+
+    const usuario = await Usuario.findOne({ email });
+
+    if( !usuario ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo no existe'
+      });
+    }
+
+    // Confirmar si el password hace match
+    const validPassword = bcrypt.compareSync( password, usuario.password );
+
+    if( !validPassword ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El password no es válido'
+      });
+    }
+
+    // Generar el JWT
+    const token = await generarJWT( usuario.id, usuario.name );
+
+    // Respuesta del servicio (la palabra return no es necesaria/obligatoria)
+    return res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+      token
+    });
+
+
+  } catch(error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    });
+  }
+
 };
 
 const revalidarToken = (req, res = response) => {
