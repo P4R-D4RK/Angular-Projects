281237f6d10a01e9881413cfffc0ecf74eb134ce
Luis Polina <luis.282001@gmail.com>
2022-09-02 17:42:20 -0500

    deployed on heroku

--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const { dbConnection } = require("./db/config");
 require("dotenv").config();
 
@@ -20,6 +22,11 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
+
+// Manejar demas rutas
+app.get( '*', ( req, res ) => {
+  res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+} );
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on the port ${process.env.PORT}`);
